00ae9991b309ab314e741cfd550ed29d8c826e27
Koki Iwata <koki.iwata@bizreach.co.jp>
2016-12-28 11:36:18 +0900

    変な継承してたのを修正

--- src/main/java/com/apifb/app/web/register/RegisterAction.java
+++ src/main/java/com/apifb/app/web/register/RegisterAction.java
@@ -1,14 +1,16 @@
 package com.apifb.app.web.register;
 
-import com.apifb.ApifbBoot;
+import com.apifb.app.web.base.ApifbBaseAction;
+import org.lastaflute.web.Execute;
 import org.lastaflute.web.response.HtmlResponse;
 
 /**
  * @author koki.iwata
  */
-public class RegisterAction extends ApifbBoot {
-    @Excute
-    public HtmlResponse index(Integer puroductId, RgisterForm form) {
+public class RegisterAction extends ApifbBaseAction {
+
+    @Execute
+    public HtmlResponse index(Integer puroductId, RegisterForm form) {
         validate(form, messages -> {}, () -> {
             return asHtml(path_Register_RegisterHtml);
         });
